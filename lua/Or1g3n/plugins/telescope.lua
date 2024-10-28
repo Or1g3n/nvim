e3a855f21845cad2343e23c068a063b23a8075ea
Or1g3n <chrisnicholas90@gmail.com>
2024-10-27 21:55:35 -0400

    changed telescope find files to <leader><leader> and added <esc> keymap to close picker

--- lua/Or1g3n/plugins/telescope.lua
+++ lua/Or1g3n/plugins/telescope.lua
@@ -14,6 +14,7 @@ return {
 	    defaults = {
 		mappings = {
 		    i = {
+			["<esc>"] = require('telescope.actions').close, -- close telescope picker
 			['<C-k>'] = actions.move_selection_previous, -- move to prev result
 			['<C-j>'] = actions.move_selection_next, -- move to next result
 			['<C-q>'] = actions.send_selected_to_qflist + actions.open_qflist,
@@ -27,11 +28,12 @@ return {
 	-- set keymaps
 	local map = vim.keymap
 
-	map.set('n', '<leader>ff', ':Telescope find_files<CR>', { desc = 'Fuzzy find files in cwd' })
-	map.set('n', '<leader>fr', ':Telescope oldfiles<CR>', { desc = 'Fuzzy find recent files' })
-	map.set('n', '<leader>fs', ':Telescope live_grep<CR>', { desc = 'Fuzzy find string in cwd' })
+	map.set('n', '<leader><leader>', ':Telescope find_files<CR>', { desc = 'Fuzzy find files in cwd' })
+	map.set('n', '<leader>fb', ':Telescope buffers<CR>', { desc = 'Fuzzy find open buffers' })
 	map.set('n', '<leader>fc', ':Telescope grep_string<CR>', { desc = 'Fuzzy string under cursor in cwd' })
 	map.set('n', '<leader>fh', ':Telescope help_tags<CR>', { desc = 'Fuzzy find help tags' })
+	map.set('n', '<leader>fr', ':Telescope oldfiles<CR>', { desc = 'Fuzzy find recent files' })
+	map.set('n', '<leader>fs', ':Telescope live_grep<CR>', { desc = 'Fuzzy find string in cwd' })
 
     end
 }

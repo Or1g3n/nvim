6e464f36097471371443427dfcbe9ac0e8edff84
Christopher Nicholas <christopher.nicholas@l3harris.com>
2024-11-28 12:58:24 -0500

    awesome update to molten-nvim. improved behavior for evalvisual. added keymap for reevalall. add custom function to auto identify cells based on markdown tags and then auto-run or select.

--- lua/Or1g3n/plugins/molten-nvim.lua
+++ lua/Or1g3n/plugins/molten-nvim.lua
@@ -19,13 +19,54 @@ return {
 	map.set("n", "<A-r><A-e>", ":MoltenEvaluateOperator<CR>", { silent = true, desc = "Molten: Run operator selection" })
 	map.set("n", "<A-r><A-r>", ":MoltenEvaluateLine<CR>", { silent = true, desc = "Molten: Evaluate line" })
 	map.set("n", "<A-r><A-c>", ":MoltenReevaluateCell<CR>", { silent = true, desc = "Molten: Re-evaluate cell" })
-	map.set("v", "<A-r>", 	   ":<C-u>MoltenEvaluateVisual<CR>gv", { silent = true, desc = "Molten: Evaluate visual selection" })
+	map.set("n", "<A-r><A-a>", ":MoltenReevaluateAll<CR>", { silent = true, desc = "Molten: Re-evaluate all cells" })
+	map.set("v", "<A-r>", 	   ":<C-u>MoltenEvaluateVisual<CR>gv<Esc>", { silent = true, desc = "Molten: Evaluate visual selection" })
 
 	map.set("n", "<A-r><A-h>", ":MoltenHideOutput<CR>", { silent = true, desc = "Molten: Hide output" })
 	map.set("n", "<A-r><A-o>", ":noautocmd MoltenEnterOutput<CR>", { silent = true, desc = "Molten: Show/enter output" })
 
 	map.set("n", "<A-r><A-n>", ":MoltenNext<CR>", { silent = true, desc = "Molten: Goto next cell" })
 	map.set("n", "<A-r><A-p>", ":MoltenPrev<CR>", { silent = true, desc = "Molten: Goto previous cell" })
+
+	-- Define function to auto-find cells
+	local function run_cell(auto_run)
+	    auto_run = auto_run or false -- default to false
+
+	    -- Set search term to markdown header to easily navigate cells with n, N
+	    vim.cmd(":let @/ = '# %%'")
+
+	    local cur_line = vim.api.nvim_get_current_line()
+	    local start_pos = nil
+
+	    -- Save the cursor position
+	    if cur_line:match("^# %%") then
+		start_pos = vim.fn.line(".")
+	    else
+		start_pos = vim.fn.search("^# %%", "b") -- Search backward for # %%
+	    end
+
+	    local end_pos = vim.fn.search("^# %%", "W") -- Search forward for # %%
+	    if end_pos == 0 then
+		end_pos = vim.fn.line("$") -- Select till end of file if no marker below
+	    else
+		end_pos = end_pos - 2
+	    end
+
+	    if start_pos == 0 then
+		print("No cell marker found above")
+		return
+	    end
+
+	    -- If visual_only then select cell range else auto-run cell
+	    if auto_run then
+		vim.fn.MoltenEvaluateRange(start_pos,end_pos)
+	    else
+		vim.api.nvim_feedkeys(start_pos .. "G V " .. end_pos .. "G", 'n' , false)
+	    end
+	end
+
+	map.set( "n", "<A-r><A-b>", function() run_cell(true) end, { silent = true, desc = "Molten: Auto-identify and run cell" })
+	map.set( "n", "<A-r><A-g>", function() run_cell(false) end, { silent = true, desc = "Molten: Auto-identify and select cell" })
 
     end,
 }

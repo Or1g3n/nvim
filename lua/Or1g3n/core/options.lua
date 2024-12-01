0e62b99f85af7e428a51d6fd4f67859f70258e58
Or1g3n <chrisnicholas90@gmail.com>
2024-11-30 21:40:42 -0500

    Eureka! updated shell options to fix nushell behavior with plugins like jupytext

--- lua/Or1g3n/core/options.lua
+++ lua/Or1g3n/core/options.lua
@@ -20,7 +20,10 @@ local options = {
     -- File
     title = false,
     --Shell
-    sh = "nu.exe"
+    sh = "nu.exe",
+    -- shellcmdflag = "-c", --Ensures that NuShell runs commands in the proper context
+    -- shellxquote = "",  -- Prevents extra quoting issues
+    -- shellslash = true -- Forces Neovim to use forward slashes
 }
 -- Set all options
 for option, value in pairs(options) do vim.opt[option] = value end

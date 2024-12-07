76b868341e2c42bd78d519c0632a2bbb7bb7e4b6
Or1g3n <chrisnicholas90@gmail.com>
2024-12-07 09:29:07 -0500

    added user command to initiate new jupyter notebook file

--- lua/Or1g3n/core/commands/init.lua
+++ lua/Or1g3n/core/commands/init.lua
@@ -1,3 +1,4 @@
-require('Or1g3n.core.commands.todos')
-require('Or1g3n.core.commands.markdown')
 require('Or1g3n.core.commands.autocommands')
+require('Or1g3n.core.commands.markdown')
+require('Or1g3n.core.commands.todos')
+require('Or1g3n.core.commands.notebooks')

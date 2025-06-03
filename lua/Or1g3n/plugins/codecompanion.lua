bc31ebaf0e5947106a93d804510172e952d6b565
Christopher Nicholas <christopher.nicholas@l3harris.com>
2025-06-03 09:19:32 -0400

    nvim(codecompanion): added support for custom adapter (laci)

--- lua/Or1g3n/plugins/codecompanion.lua
+++ lua/Or1g3n/plugins/codecompanion.lua
@@ -1,22 +1,37 @@
 return {
-    "olimorris/codecompanion.nvim",
-    dependencies = {
-	"nvim-lua/plenary.nvim",
-	"nvim-treesitter/nvim-treesitter",
+  "olimorris/codecompanion.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-treesitter/nvim-treesitter",
+  },
+  opts = {
+    adapters = {
+      laci_pixtral = function()
+        return require("codecompanion.adapters").extend("openai_compatible", {
+          env = {
+            url             = "https://api.laci.l3harris.com",
+            api_key         = os.getenv("LACI_API_KEY"),
+            chat_url        = "/v1/chat/completions",
+            models_endpoint = "/v1/models",
+          },
+          schema = {
+            model = {
+              default = "mistralai/Pixtral-12B-2409",
+            },
+          },
+        })
+      end,
+    },
+    strategies = {
+      chat = {
+        adapter = "laci_pixtral",   -- or "laci_pixtral" if you want this model by default
+      },
+      inline = {
+        adapter = "laci_pixtral",
+      },
     },
     opts = {
-	strategies = {
-	    -- Change the default chat adapter
-	    chat = {
-		adapter = "copilot",
-	    },
-	    inline = {
-		adapter = "copilot",
-	    },
-	},
-	opts = {
-	    -- Set debug logging
-	    log_level = "DEBUG",
-	},
+      log_level = "DEBUG",
     },
+  },
 }

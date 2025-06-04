d285820bbe56c1c4d2906457c50c06a1b476f4d1
Or1g3n <chrisnicholas90@gmail.com>
2025-06-04 19:58:39 -0400

    nvim(codecompanion): added autocheck for vpn to determine model

--- lua/Or1g3n/plugins/codecompanion.lua
+++ lua/Or1g3n/plugins/codecompanion.lua
@@ -1,37 +1,50 @@
+local function on_company_vpn()
+    local handle = io.popen('ping -n 1 api.laci.l3harris.com')
+    if not handle then
+	return false
+    end
+    local result = handle:read("*a")
+    handle:close()
+    return result and result:find("TTL=") ~= nil
+end
+
+local default_adapter = on_company_vpn() and "laci_pixtral" or "copilot"
+
 return {
-  "olimorris/codecompanion.nvim",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-treesitter/nvim-treesitter",
-  },
-  opts = {
-    adapters = {
-      laci_pixtral = function()
-        return require("codecompanion.adapters").extend("openai_compatible", {
-          env = {
-            url             = "https://api.laci.l3harris.com",
-            api_key         = os.getenv("LACI_API_KEY"),
-            chat_url        = "/v1/chat/completions",
-            models_endpoint = "/v1/models",
-          },
-          schema = {
-            model = {
-              default = "mistralai/Pixtral-12B-2409",
-            },
-          },
-        })
-      end,
-    },
-    strategies = {
-      chat = {
-        adapter = "laci_pixtral",   -- or "laci_pixtral" if you want this model by default
-      },
-      inline = {
-        adapter = "laci_pixtral",
-      },
+    "olimorris/codecompanion.nvim",
+    dependencies = {
+	"nvim-lua/plenary.nvim",
+	"nvim-treesitter/nvim-treesitter",
     },
     opts = {
-      log_level = "DEBUG",
+	adapters = {
+	    laci_pixtral = function()
+		return require("codecompanion.adapters").extend("openai_compatible", {
+		    env = {
+			url             = "https://api.laci.l3harris.com",
+			api_key         = os.getenv("LACI_API_KEY"),
+			chat_url        = "/v1/chat/completions",
+			models_endpoint = "/v1/models",
+		    },
+		    schema = {
+			model = {
+			    default = "mistralai/Pixtral-12B-2409",
+			},
+		    },
+		})
+	    end,
+	    -- copilot adapter assumed to be configured elsewhere
+	},
+	strategies = {
+	    chat = {
+		adapter = default_adapter,
+	    },
+	    inline = {
+		adapter = default_adapter,
+	    },
+	},
+	opts = {
+	    log_level = "DEBUG",
+	},
     },
-  },
 }

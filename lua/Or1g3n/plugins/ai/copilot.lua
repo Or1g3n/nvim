46f9c0a630d43f03f1f5c30896280695a225472b
Christopher Nicholas <chrisnicholas90@gmail.com>
2026-02-15 11:36:42 -0500

    nvim(copilot): add keymaps for accepting word and line suggestions
    - Added insert mode keymaps: `<C-Right>` to accept the next Copilot word, and `<S-Right>` to accept the next Copilot line.
    - Improved code structure and comments for Copilot plugin config.

--- lua/Or1g3n/plugins/ai/copilot.lua
+++ lua/Or1g3n/plugins/ai/copilot.lua
@@ -1,17 +1,26 @@
 -- Make sure to run :Copilot auth to authenticate with GitHub Copilot
 
 return {
-    'github/copilot.vim',
-    enabled = true,
-    config = function()
-	vim.g.copilot_enabled = false
-	vim.keymap.set('n', '<leader>cp', function()
-	    vim.g.copilot_enabled = not vim.g.copilot_enabled
-	    if vim.g.copilot_enabled then
-		vim.notify("Copilot inline suggestions enabled", vim.log.levels.INFO)
-	    else
-		vim.notify("Copilot inline suggestions disabled", vim.log.levels.INFO)
-	    end
-	end, { desc = "Copilot: Toggle Copilot suggestions" })
-    end,
+	'github/copilot.vim',
+	enabled = true,
+	config = function()
+		vim.g.copilot_enabled = false
+
+		-- Toggle Copilot suggestions
+		vim.keymap.set('n', '<leader>cp', function()
+			vim.g.copilot_enabled = not vim.g.copilot_enabled
+			if vim.g.copilot_enabled then
+				vim.notify("Copilot inline suggestions enabled", vim.log.levels.INFO)
+			else
+				vim.notify("Copilot inline suggestions disabled", vim.log.levels.INFO)
+			end
+		end, { desc = "Copilot: Toggle Copilot suggestions" })
+
+		-- Accept word: <C-Right>
+		vim.keymap.set('i', '<C-Right>', 'copilot#AcceptWord("<C-Right>")', { expr = true, replace_keycodes = false, desc = "Copilot: Accept word" })
+
+		-- Accept line: <S-Right>
+		vim.keymap.set('i', '<S-Right>', 'copilot#AcceptLine("<S-Right>")', { expr = true, replace_keycodes = false, desc = "Copilot: Accept line" })
+
+	end,
 }

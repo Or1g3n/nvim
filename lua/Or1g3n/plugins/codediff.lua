50a7b95ac3f41aea1c76252f4682739420ab30aa
Christopher Nicholas <chrisnicholas90@gmail.com>
2026-02-27 17:46:13 -0500

    nvim(codediff): add keybindings for history and diff against HEAD
    Added two normal mode keybindings: <A-c><A-h> to show CodeDiff history, and <A-c><A-d> to show diff against HEAD. This improves accessibility of CodeDiff commands.

--- lua/Or1g3n/plugins/codediff.lua
+++ lua/Or1g3n/plugins/codediff.lua
@@ -2,4 +2,8 @@ return {
 	"esmuellert/codediff.nvim",
 	enabled = true,
 	cmd = "CodeDiff",
+    keys = {
+        { mode = 'n', "<A-c><A-h>", "<cmd>CodeDiff history<cr>", desc = "CodeDiff: show history" },
+        { mode = 'n', "<A-c><A-d>", "<cmd>CodeDiff HEAD<cr>", desc = "CodeDiff: show diff against HEAD" },
+    }
 }

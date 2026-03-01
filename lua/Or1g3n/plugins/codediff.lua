8e57e6b9f1e1d9df1cc00c75b8738cbb06083c4b
Christopher Nicholas <chrisnicholas90@gmail.com>
2026-03-01 11:44:50 -0500

    nvim(codediff): add side-by-side diff layout option
    Added the `opts.diff.layout` configuration to set the diff layout for codediff.nvim. The default is now "side-by-side", with an inline option commented for reference.

--- lua/Or1g3n/plugins/codediff.lua
+++ lua/Or1g3n/plugins/codediff.lua
@@ -2,8 +2,14 @@ return {
 	"esmuellert/codediff.nvim",
 	enabled = true,
 	cmd = "CodeDiff",
-    keys = {
-        { mode = 'n', "<A-c><A-h>", "<cmd>CodeDiff history<cr>", desc = "CodeDiff: show history" },
-        { mode = 'n', "<A-c><A-d>", "<cmd>CodeDiff HEAD<cr>", desc = "CodeDiff: show diff against HEAD" },
-    }
+	keys = {
+		{ mode = 'n', "<A-c><A-h>", "<cmd>CodeDiff history<cr>", desc = "CodeDiff: show history" },
+		{ mode = 'n', "<A-c><A-d>", "<cmd>CodeDiff HEAD<cr>", desc = "CodeDiff: show diff against HEAD" },
+	},
+	opts = {
+		diff = {
+			layout = "side-by-side",             -- Diff layout: "side-by-side" (two panes) or "inline" (single pane with virtual lines)
+			-- layout = "inline",             -- Diff layout: "side-by-side" (two panes) or "inline" (single pane with virtual lines)
+		},
+	},
 }
